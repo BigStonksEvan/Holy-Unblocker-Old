2ed238f80374a5275ecbad433f6e4ab831b2dab2
BigStonksEvan <99767834+BigStonksEvan@users.noreply.github.com>
2022-03-28 23:11:08 -0700

    Update prset.js

--- views/assets/js/prset.js
+++ views/assets/js/prset.js
@@ -10,7 +10,7 @@ function prSet(type) {
     prGo2 = document.getElementById("pr-go2");
 
     prUrl.addEventListener("keydown", function(e) {
-        if (e.code == "Tab" && event.shiftKey && event.keyCode == 9 && prUrl.value) goProx[type](prUrl.value, true);
+        if (e.code == "Enter" && event.shiftKey && e.code == "Ctrl" && prUrl.value) goProx[type](prUrl.value, true);
     }, false);
 
     prGo1.addEventListener("click", function() {
